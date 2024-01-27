9cdf4b662e847f628e35070e0eee07d6fc153754
joeyagreco <joeyagreco@gmail.com>
2024-01-27 12:39:29 -0600

    logger stuff

--- discbase/run.py
+++ discbase/run.py
@@ -2,10 +2,12 @@ import asyncio
 
 import discord
 from dotenv import load_dotenv
+from util.CustomLogger import CustomLogger
 from util.EnvironmentReader import EnvironmentReader
 
 
 def main():
+    logger = CustomLogger.get_logger()
     load_dotenv()
     TOKEN = EnvironmentReader.get("BOT_TOKEN")
     GUILD_ID = EnvironmentReader.get("GUILD_ID", as_type=int)
@@ -16,7 +18,7 @@ def main():
 
     @client.event
     async def on_ready():
-        print(f"{client.user} has connected to Discord!")
+        logger.important(f"DISCBASE IS RUNNING WITH USER {client.user}")
 
     client.run(TOKEN)
 
