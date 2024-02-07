9da69ac87ac9520cfdeb2720cee5932b39d4cc23
joeyagreco <joeyagreco@gmail.com>
2024-02-06 23:34:41 -0600

    speed test

--- test_e2e/test_dump.py
+++ test_e2e/test_dump.py
@@ -2,94 +2,69 @@ import asyncio
 import os
 import unittest
 
-from test_e2e.util import client_shutdown_successfully, get_client
+from test_e2e.util import get_client, run_async_test
 
 
 class TestDump(unittest.TestCase):
     RESOURCES_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "resources"))
 
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.client = get_client()
+        cls.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(cls.loop)
+        cls.loop.run_until_complete(cls.client.start())
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.loop.run_until_complete(cls.client.stop())
+        cls.loop.close()
+
     def test_dump_text(self):
-        client = get_client()
-
         async def main():
-            await client.start()
-            try:
-                self.stored_record = await client.dump(value="foo")
-            except Exception as e:
-                self.fail(e)
-            await client.stop()
+            stored_record = await self.client.dump(value="foo")
+            self.assertEqual("foo", stored_record.text_data)
+            self.assertIsNotNone(stored_record.record_id)
+            self.assertEqual([], stored_record.media_urls)
+            self.assertIsNotNone(stored_record.created_at_utc)
+            self.assertIsNotNone(stored_record.discord_message)
 
-        asyncio.run(main())
-
-        self.assertEqual("foo", self.stored_record.text_data)
-        self.assertIsNotNone(self.stored_record.record_id)
-        self.assertEqual([], self.stored_record.media_urls)
-        self.assertIsNotNone(self.stored_record.created_at_utc)
-        self.assertIsNotNone(self.stored_record.discord_message)
-        self.assertTrue(client_shutdown_successfully(client))
+        run_async_test(self, main())
 
     def test_dump_image_local(self):
-        client = get_client()
-
-        local_image_path = os.path.join(self.RESOURCES_PATH, "image.jpg")
-
         async def main():
-            await client.start()
-            try:
-                self.stored_record = await client.dump(media_path=local_image_path)
-            except Exception as e:
-                self.fail(e)
-            await client.stop()
+            local_image_path = os.path.join(self.RESOURCES_PATH, "image.jpg")
+            stored_record = await self.client.dump(media_path=local_image_path)
+            self.assertEqual("", stored_record.text_data)
+            self.assertIsNotNone(stored_record.record_id)
+            self.assertEqual(1, len(stored_record.media_urls))
+            self.assertIsNotNone(stored_record.created_at_utc)
+            self.assertIsNotNone(stored_record.discord_message)
 
-        asyncio.run(main())
-
-        self.assertEqual("", self.stored_record.text_data)
-        self.assertIsNotNone(self.stored_record.record_id)
-        self.assertEqual(1, len(self.stored_record.media_urls))
-        self.assertIsNotNone(self.stored_record.created_at_utc)
-        self.assertIsNotNone(self.stored_record.discord_message)
-        self.assertTrue(client_shutdown_successfully(client))
+        run_async_test(self, main())
 
     def test_dump_image_online(self):
-        client = get_client()
-
         async def main():
-            await client.start()
-            try:
-                self.stored_record = await client.dump(
-                    media_path="https://en.wikipedia.org/wiki/Discord#/media/File:Discord_logo.svg"
-                )
-            except Exception as e:
-                self.fail(e)
-            await client.stop()
+            stored_record = await self.client.dump(
+                media_path="https://en.wikipedia.org/wiki/Discord#/media/File:Discord_logo.svg"
+            )
+            self.assertEqual("", stored_record.text_data)
+            self.assertIsNotNone(stored_record.record_id)
+            self.assertEqual(1, len(stored_record.media_urls))
+            self.assertIsNotNone(stored_record.created_at_utc)
+            self.assertIsNotNone(stored_record.discord_message)
 
-        asyncio.run(main())
-
-        self.assertEqual("", self.stored_record.text_data)
-        self.assertIsNotNone(self.stored_record.record_id)
-        self.assertEqual(1, len(self.stored_record.media_urls))
-        self.assertIsNotNone(self.stored_record.created_at_utc)
-        self.assertIsNotNone(self.stored_record.discord_message)
-        self.assertTrue(client_shutdown_successfully(client))
+        run_async_test(self, main())
 
     def test_dump_text_and_image(self):
-        client = get_client()
-
         local_image_path = os.path.join(self.RESOURCES_PATH, "image.jpg")
 
         async def main():
-            await client.start()
-            try:
-                self.stored_record = await client.dump(value="foo", media_path=local_image_path)
-            except Exception as e:
-                self.fail(e)
-            await client.stop()
+            stored_record = await self.client.dump(value="foo", media_path=local_image_path)
+            self.assertEqual("foo", stored_record.text_data)
+            self.assertIsNotNone(stored_record.record_id)
+            self.assertEqual(1, len(stored_record.media_urls))
+            self.assertIsNotNone(stored_record.created_at_utc)
+            self.assertIsNotNone(stored_record.discord_message)
 
-        asyncio.run(main())
-
-        self.assertEqual("foo", self.stored_record.text_data)
-        self.assertIsNotNone(self.stored_record.record_id)
-        self.assertEqual(1, len(self.stored_record.media_urls))
-        self.assertIsNotNone(self.stored_record.created_at_utc)
-        self.assertIsNotNone(self.stored_record.discord_message)
-        self.assertTrue(client_shutdown_successfully(client))
+        run_async_test(self, main())
